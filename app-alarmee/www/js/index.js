b146d9e4236058c615aca36e6a2708befe0d7a7d
agustinbaretto <agustinbaretto@gmail.com>
2014-07-29 22:09:32 +0000

    qq

--- app-alarmee/www/js/index.js
+++ app-alarmee/www/js/index.js
@@ -15,17 +15,24 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
+        navigator.geolocation.getCurrentPosition(this.onGeoSuccess, this.onGeoError);
         alert("hola");
     },
     // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+    onGeoSuccess: function(position) {
+    alert('Latitude: '          + position.coords.latitude          + '\n' +
+          'Longitude: '         + position.coords.longitude         + '\n' +
+          'Altitude: '          + position.coords.altitude          + '\n' +
+          'Accuracy: '          + position.coords.accuracy          + '\n' +
+          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+          'Heading: '           + position.coords.heading           + '\n' +
+          'Speed: '             + position.coords.speed             + '\n' +
+          'Timestamp: '         + position.timestamp                + '\n');
+    },
+    // onError Callback receives a PositionError object
+    //
+    onGeoError: function(error) {
+    alert('code: '    + error.code    + '\n' +
+          'message: ' + error.message + '\n');
     }
 };
