0a7ed9d8bc77b0c4867ca5179cd69129cdbadc0a
agustinbaretto <agustinbaretto@gmail.com>
2014-07-30 14:06:27 +0000

    q

--- js/index.js
+++ js/index.js
@@ -1,31 +1,30 @@
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        navigator.geolocation.getCurrentPosition(this.onGeoSuccess, this.onGeoError);
-        alert("hola");
-    },
-    // Update DOM on a Received Event
-    onGeoSuccess: function(position) {
-    alert('Latitude: '          + position.coords.latitude          + '\n');
-    },
-    // onError Callback receives a PositionError object
-    //
-    onGeoError: function(error) {
+// Wait for Cordova to load
+//
+document.addEventListener("deviceready", onDeviceReady, false);
+
+// Cordova is ready
+//
+function onDeviceReady() {
+    navigator.geolocation.getCurrentPosition(onSuccess, onError);
+}
+
+// onSuccess Geolocation
+//
+function onSuccess(position) {
+    var element = document.getElementById('geolocation');
+    element.innerHTML = 'Latitude: '           + position.coords.latitude              + '<br />' +
+                        'Longitude: '          + position.coords.longitude             + '<br />' +
+                        'Altitude: '           + position.coords.altitude              + '<br />' +
+                        'Accuracy: '           + position.coords.accuracy              + '<br />' +
+                        'Altitude Accuracy: '  + position.coords.altitudeAccuracy      + '<br />' +
+                        'Heading: '            + position.coords.heading               + '<br />' +
+                        'Speed: '              + position.coords.speed                 + '<br />' +
+                        'Timestamp: '          +                                   position.timestamp          + '<br />';
+}
+
+// onError Callback receives a PositionError object
+//
+function onError(error) {
     alert('code: '    + error.code    + '\n' +
-          'message: ' + error.message + '\n');
-    }
-};
+            'message: ' + error.message + '\n');
+}
